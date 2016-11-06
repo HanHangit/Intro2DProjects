ffaa43ec54a9e87bede5bbe8c4797c1da7a8994f
wookiepeter <matthis.hagen@o2online.de>
2016-11-06 17:14:12 +0100

    redid collision map -> 3;

---
+++ 3. Finished Intro-02.11/Map_Collision/Game1.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Map_Collision
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Player player;
+        Tilemap tileMap;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            player = new Player(Content.Load<Texture2D>("human"), new Vector2(100, 100), 1f, 100f);
+            tileMap = new Tilemap(new Texture2D[] { Content.Load<Texture2D>("grass"), Content.Load<Texture2D>("rock") }, Content.Load<Texture2D>("bitmap"), 16);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            tileMap.Update(gameTime);
+            player.Update(gameTime, tileMap);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            // TODO: Add your drawing code here
+            tileMap.Draw(spriteBatch);
+            player.Draw(spriteBatch);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ 3. Finished Intro-02.11/Map_Collision/Tilemap.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Map_Collision
+{
+    class Tilemap
+    {
+        Tile[,] tileMap;
+        int tileSize;
+
+        public Tilemap(Texture2D[] textures, Texture2D bitMap, int _tileSize)
+        {
+            tileSize = _tileSize;
+            tileMap = new Tile[bitMap.Width, bitMap.Height];
+
+            BuildMap(textures, bitMap);
+        }
+
+        private void BuildMap(Texture2D[] textures, Texture2D bitMap)
+        {
+            Color[] colores = new Color[bitMap.Width * bitMap.Height];
+
+            bitMap.GetData(colores);
+
+            for(int y = 0; y < tileMap.GetLength(1); y++)
+            {
+                for (int x = 0; x <tileMap.GetLength(0); x++)
+                {
+                    if(colores[y * tileMap.GetLength(0) + x] == Color.White)
+                    {
+                        // Grass
+                        tileMap[x, y] = new Tile(textures[0], new Vector2(x * tileSize, y * tileSize), 0);
+                    }
+                    else
+                    {
+                        // Stein
+                        tileMap[x, y] = new Tile(textures[1], new Vector2(x * tileSize, y * tileSize), 1);
+                    }
+                }
+            }
+        }
+
+        public bool Walkable(Vector2 currentPosition)
+        {
+            return tileMap[(int)(currentPosition.X / tileSize), (int)(currentPosition.Y / tileSize)].Walkable();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int y = 0; y < tileMap.GetLength(1); y++)
+            {
+                for(int x = 0; x <tileMap.GetLength(0); x++)
+                {
+                    tileMap[x, y].Draw(spriteBatch);
+                }
+            }
+        }
+    }
+}
